3633933e84686fce14d56a81b234d7da0a2885ed
Andrew Bilous <bilous.a.work@gmail.com>
2024-05-08 01:00:06 -0400

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,13 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap' // Import all of Bootstrap's JS
+import cardComponent from "./cardComponent";
+import {fetchItems} from "./api";
+
+(async function renderItems() {
+  try {
+    const data = await fetchItems();
+    const itemsHTML = data.map(item => cardComponent(item));
+    document.getElementById('cards').innerHTML = itemsHTML.join('');
+  } catch (error) {
+    console.error('Error rendering items:', error);
+  }
+})()
